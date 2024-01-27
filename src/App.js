5230a0774f9e47952e1882862386f253ea58ab25
themightyvicki1 <victoria.greer861@gmail.com>
2024-01-26 16:51:32 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
-
+//create structure of page here
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app ☁</h1>
-      <footer>
-        <a
-          href="https://github.com/themightyvicki1/weather-app-using-react"
-          title="</div>"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Coded by{" "}
-        </a>{" "}
-        Victoria Greer
-      </footer>
+      {/*this container is created b/c of using bootstrap*/}
+      <div className="container">
+        <h1>Weather app ☁</h1>
+        <footer>
+          <a
+            href="https://github.com/themightyvicki1/weather-app-using-react"
+            title="</div>"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Coded by{" "}
+          </a>{" "}
+          Victoria Greer
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//this is where we import bootstrap, to link it to the app
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
