5486fe232d850b6935901eafcaa8d1ddce7b3158
themightyvicki1 <victoria.greer861@gmail.com>
2024-01-26 16:40:10 -0800

    added footer and link to github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app ☁</div>;
+  return (
+    <div className="App">
+      <h1>Weather app ☁</h1>
+      <footer>
+        <a
+          href="https://github.com/themightyvicki1/weather-app-using-react"
+          title="</div>"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Coded by{" "}
+        </a>{" "}
+        Victoria Greer
+      </footer>
+    </div>
+  );
 }
